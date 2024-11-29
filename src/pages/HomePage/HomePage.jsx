ebc6a3414b707ec7c1fc4b9a4488230f438cbd87
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-11-29 14:21:59 -0800

    eagle to the fornt page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,38 @@
 
 import './HomePage.css'
 import NavBar from "../../components/NavBar/NavBar"
+import { useEffect, useState } from 'react'
+import { supabase } from '../../services/supabase';
 
 export default function HomePage() {
+
+  const [ image, setImage ] = useState();
+
+  useEffect(() => {
+
+    const fetchEagle = async () => {
+      const { data, error } =  await supabase.storage.from('images').getPublicUrl('eagle-photos/eagle.jpg')
+      setImage(data.publicUrl)
+
+    }
+
+    fetchEagle()
+     
+  }, [])
+
+
+
+
+
   return (
-    <div>
-      
+    <div style={{ width: '100%'}}>
+     { image ?  <img src={image} className='eagleBackground' /> : 'loading'}
         <NavBar />
 
      
 
         <div className='homePageContainer'>
-          Home Page
+        
         </div>
     </div>
   )
