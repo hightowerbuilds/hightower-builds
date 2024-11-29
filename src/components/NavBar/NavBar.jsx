d5aa01a4e687e0ee3c83b34a7eb8d2cb411fb2b0
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-11-29 09:27:57 -0800

    setting up navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,26 +7,54 @@ import { supabase } from '../../services/supabase';
 export default function NavBar() {
 
 
-  const [ image, setImage ] = useState();
+  const [ homeButton, setHomeButton ] = useState();
+  const [ clothingButton, setClothingButton ] = useState();
+  const [ webDevButton, setWebDevButton ] = useState();
+  const [ contactButton, setContactButton ] = useState();
+  
 
 
   useEffect(() => {
-    const { data } = supabase.storage
+    const fetchHomeButton = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('home-sketch.png')
+      setHomeButton(data.publicUrl)
+    }
+    
+    const fetchClothingButton = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('clothing-sketch.png')
+      setClothingButton(data.publicUrl)
+    }
+
+    const fetchWebDevButton = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('webdev-sketch.png')
+      setWebDevButton(data.publicUrl)
+    }
+
+    const fetchContactButton = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('contact-sketch.png')
+      setContactButton(data.publicUrl)
+    }
+
+    fetchHomeButton();
+    fetchClothingButton();
+    fetchWebDevButton();
+    fetchContactButton();
+
   }, [])
 
-
+ 
 
 
 
 
   return (
  <div className="navBarContainer">
-  <img src="" alt="" />
-    <NavLink to='/'>Home</NavLink>
-    <NavLink to='/clothing'>Clothing</NavLink>
-    <NavLink to='/webdev'>Web Development</NavLink>
-    <NavLink to='/contact'>Contact</NavLink>
-   
+
+    <NavLink to='/'>{homeButton ? <img className='homeButton' src={homeButton}></img>: 'loading'}</NavLink>
+    <NavLink to='/clothing'>{clothingButton ? <img className='clothingButton' src={clothingButton}></img> : 'loading'}</NavLink>
+    <NavLink to='/webdev'>{ webDevButton ? <img className='webDevButton' src={webDevButton}/> : 'loading'}</NavLink>
+    <NavLink to='/contact'>{contactButton ? <img className='contactButton' src={contactButton} /> : 'loading'}</NavLink>
+
  </div>
   )
 }
