7c3bcdd96188e23842f0bca46d9b1e0066ff12ed
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-11-29 14:38:47 -0800

    styles home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,16 +7,22 @@ import { supabase } from '../../services/supabase';
 export default function HomePage() {
 
   const [ image, setImage ] = useState();
+  const [ imageTwo, setImageTwo ] = useState();
 
   useEffect(() => {
 
     const fetchEagle = async () => {
       const { data, error } =  await supabase.storage.from('images').getPublicUrl('eagle-photos/eagle.jpg')
       setImage(data.publicUrl)
+    }
 
+    const fetchEagleOnStick = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('eagle-photos/eagle-stick.jpg') 
+      setImageTwo(data.publicUrl)
     }
 
     fetchEagle()
+    fetchEagleOnStick()
      
   }, [])
 
@@ -26,14 +32,14 @@ export default function HomePage() {
 
   return (
     <div style={{ width: '100%'}}>
-     { image ?  <img src={image} className='eagleBackground' /> : 'loading'}
+
+      { image ?  <img src={image} className='eagleBackground' /> : 'loading'}
+      { imageTwo ?  <img src={imageTwo} className='eagleStickBackground' /> : 'loading'}
         <NavBar />
 
-     
+      <div className='homePageContainer'></div>
 
-        <div className='homePageContainer'>
-        
-        </div>
+
     </div>
   )
 }
