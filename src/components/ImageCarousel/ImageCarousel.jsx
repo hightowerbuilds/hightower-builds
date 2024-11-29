8e7e5ccee64f5ac98211394e0a6f6b2ccdd6aadb
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-11-28 19:13:41 -0800

    getting supabase hooked up

--- src/components/ImageCarousel/ImageCarousel.jsx
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -3,59 +3,30 @@ import './ImageCarousel.css'
 import { useState, useEffect } from 'react'
 
 export default function ImageCarousel() {
-  const [images, setImages] = useState([]);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+ const [images, setImages] = useState();
 
   useEffect(() => {
+
     const fetchImages = async () => {
-      try {
-        const { data, error } = await supabase.storage.from('images').list();
-        console.log(data)
-        if (error) {
-          console.error('Error fetching images:', error);
-        } else {
-          const imageUrls = await Promise.all(
-            data.map(async (image) => {
-              const { data: publicUrlData } = await supabase.storage
-                .from('images')
-                .getPublicUrl(image.name);
-              return publicUrlData.publicUrl;
-            })
-          );
-          setImages(imageUrls);
-        }
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      }
-    };
+      const { data } = supabase.storage.from('images').getPublicUrl('eagle.jpg')
+      setImages(data.publicUrl)
+      console.log(data.publicUrl)
+    }
+   fetchImages()
+  }, [])
 
-    fetchImages();
-  }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex < images.length - 1 ? prevIndex + 1 : 0
-      );
-    }, 3000); // Change image every 3 seconds
-
-    return () => clearInterval(interval);
-  }, [images]);
 
   return (
     <div>
-      {images.length > 0 ? (
-        <img
-          style={{
-            height: 'auto',
-            width: '100%',
-          }}
-          src={images[currentImageIndex]}
-          alt={`Image ${currentImageIndex + 1}`}
-        />
-      ) : (
-        <p>Loading images...</p>
-      )}
+      {
+        images ?  <img style={{
+          width: '100%',
+          height: 'auto'
+        }} src={images} /> : 'loading photo'
+      }
+     
     </div>
   );
 }
