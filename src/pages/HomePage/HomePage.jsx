df26ff00075e9d6b11b612a4e6add108a52c733c
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-11-29 16:53:07 -0800

    more eglae photos

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,6 +8,8 @@ export default function HomePage() {
 
   const [ image, setImage ] = useState();
   const [ imageTwo, setImageTwo ] = useState();
+  const [ eagleStickTwo, setEagleStickTwo ] = useState();
+  const [ distantEagle, setDistantEagle] = useState();
   const [currentIndex, setCurrentIndex] = useState(0);
 
 
@@ -23,12 +25,25 @@ export default function HomePage() {
       setImageTwo(data.publicUrl)
     }
 
-    fetchEagle()
-    fetchEagleOnStick()
+    const fetchEagleStickTwo = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('eagle-photos/eagle-stick-2.JPG');
+      setEagleStickTwo(data.publicUrl)
+    }
+
+    const fetchDistantEagle = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('eagle-photos/distant-eagle.JPG');
+        setDistantEagle(data.publicUrl);
+      
+    }
+
+    fetchEagle();
+    fetchEagleOnStick();
+    fetchEagleStickTwo();
+    fetchDistantEagle();
      
     }, [])
 
-    const images = [image, imageTwo]
+    const images = [image, imageTwo, eagleStickTwo, distantEagle]
 
 
 useEffect(() => {
