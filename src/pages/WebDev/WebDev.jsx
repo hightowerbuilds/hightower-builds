ee766ff2a75d20bdaac5b46dca5199d03d52ce85
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-11-30 23:01:52 -0800

    adding webdev styles

--- src/pages/WebDev/WebDev.jsx
+++ src/pages/WebDev/WebDev.jsx
@@ -1,10 +1,66 @@
 
 import './WebDev.css'
 import NavBar from "../../components/NavBar/NavBar"
+import { useEffect, useState } from 'react'
+import { supabase } from '../../services/supabase';
 
 export default function WebDev() {
+
+  const [ blueSky, setBlueSky ] = useState();
+  const [ aloePlant, setAloePlant ] = useState();
+  const [ deskLamp, setDeskLamp ] = useState();
+  const [ ceilingLight, setCeilingLight ] = useState();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchBlueSky = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('webdev/blueSky.JPG');
+      setBlueSky(data.publicUrl)
+    }
+
+    const fetchAloePlant = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('webdev/aloePlant.JPG');
+      setAloePlant(data.publicUrl)
+    }
+
+    const fetchCeilingLight = async () => {
+      const { data, error, } = await supabase.storage.from('images').getPublicUrl('webdev/ceilingLight.JPG');
+      setCeilingLight(data.publicUrl)
+    }
+
+    const fetchDeskLamp = async () => {
+      const { data, error } = await supabase.storage.from('images').getPublicUrl('webdev/deskLamp.JPG');
+      setDeskLamp(data.publicUrl);
+    }
+
+    fetchBlueSky();
+    fetchAloePlant();
+    fetchCeilingLight();
+    fetchDeskLamp();
+
+  }, [])
+
+
+  
+  const images = [ aloePlant, ceilingLight, deskLamp ]
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % images.length); 
+    }, 7000);
+  
+    return () => clearInterval(intervalId);
+  }, [images]);
+
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      backgroundImage: `url(${blueSky})`, 
+      height: '120vh',
+      width: '100%'
+    }} >
+
+      { images ?  <img src={images[currentIndex]} className='webDevBackground' /> : 'loading'}
 
       <NavBar />
 
