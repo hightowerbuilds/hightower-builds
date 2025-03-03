b72295d12b2879979a4987b584f2cecdb3252053
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-03-02 21:37:58 -0800

    building outthe tripe aspects first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import WebDev from './pages/WebDev/WebDev'
-import Painting from './pages/Clothing/Clothing'
+import Store from './pages/Clothing/Clothing'
 import HomePage from './pages/HomePage/HomePage'
 import Contact from './pages/Contact/Contact'
 import { Route, Routes } from 'react-router-dom'
@@ -12,7 +12,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path='/contact' element={<Contact />} />
       <Route path="/webdev" element={<WebDev />} />
-      <Route path="/clothing" element={<Painting />} />
+      <Route path="/clothing" element={<Store />} />
     </ Routes>
   
   )
--- client/src/pages/Clothing/Clothing.jsx
+++ client/src/pages/Clothing/Clothing.jsx
@@ -1,8 +1,11 @@
 
 import './Clothing.css'
 import NavBar from "../../components/NavBar/NavBar"
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, PaymentElement } from '@stripe/react-stripe-js'
+import axios from 'axios';
 
-export default function Painting() {
+export default function Store() {
 
 
 
