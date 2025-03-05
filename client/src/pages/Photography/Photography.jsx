52ff225aa9de12c76f2cc711a81526257e5ac1e1
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-03-05 02:35:25 -0800

    bringing photos into phot page believe it or not

--- client/src/pages/Photography/Photography.jsx
+++ client/src/pages/Photography/Photography.jsx
@@ -1,18 +1,40 @@
 
 import './Photograpy.css'
 import NavBar from "../../components/NavBar/NavBar"
-
+import { supabase } from '../../services/supabase';
+import { useState, useEffect } from 'react'
 
 export default function Photography() {
+
+    const [ photoOne, setPhotoOne ] = useState()
+
+    useEffect(() => {
+        async function fetchImages() {
+                const { data, error } = await supabase.storage.from('images').getPublicUrl('eagle-photos/eagleStalk.jpg');
+                setPhotoOne(data.publicUrl)
+          }
+      
+          fetchImages();
+    }, [])
+
+    const showImages = () => {
+        return (
+           <img style={{height: '700px'}} src={`${photoOne}`} />
+        )
+    }
+
+
+
   return (
     <div style={{
-        width: '100%'
+        width: '100%',
+
     }}>
         <NavBar />
         Photography
 
         <div className="mainContentPhotography">
-            images of whidbey
+            {showImages()}
         </div>
     </div>
   )
