2f0dd54f2871e91dd5766b8782263bb707b6aa86
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-03-22 10:03:17 -0700

    adding images to clothing page

--- client/src/pages/Clothing/Clothing.jsx
+++ client/src/pages/Clothing/Clothing.jsx
@@ -1,11 +1,21 @@
 
 import './Clothing.css'
 import NavBar from "../../components/NavBar/NavBar"
+import { useState, useEffect } from 'react'
+
 
 export default function Store() {
 
+const [arrow, setArrow] = useState(false)
 
 
+const handleArrow = () => {
+  if (arrow === false){
+    setArrow(true)
+  } else {
+    setArrow(false)
+  }
+ }
 
   return (
     <div >
@@ -13,14 +23,13 @@ export default function Store() {
       <NavBar />  
 
       <div className='mainClothingContainer'>
-      <h2>clothing</h2>
-
+        <h2>clothing</h2>
 
         <div className='clothingItemsContainer'>
           <div>
             <h4> where your pants?</h4>
 
-            <div className='itemOne'>
+            <div className='itemOne' onClick={handleArrow} >
               blue jeans
             </div>
           </div>
@@ -51,8 +60,25 @@ export default function Store() {
 
         </div>
 
-        
+        {
+          arrow ?       
+          <div className='blueArrow'>
 
+            <div className='imagesBlueArrow'>
+
+            </div>
+
+            <div className='interiorBlueArrow'>
+              <p>pants A</p>
+              <p>pants B</p>
+              <p>pants C</p>
+              <p>pants D</p>
+              <p>pants E</p>
+              <p>pants F</p>
+            </div>
+
+          </div> : ''
+        }
         
       </div>
  
