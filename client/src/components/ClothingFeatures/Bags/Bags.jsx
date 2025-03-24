99c6c19ec3dee6242b23cd500a157e7f83374d4f
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-03-23 18:34:51 -0700

    adding component

---
+++ client/src/components/ClothingFeatures/Bags/Bags.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Bags() {
+  return (
+    <div>Bags</div>
+  )
+}
---
+++ client/src/components/ClothingFeatures/Shirts/Shirts.jsx
@@ -0,0 +1,6 @@
+
+export default function Shirts() {
+  return (
+    <div>ClothingShirts</div>
+  )
+}
